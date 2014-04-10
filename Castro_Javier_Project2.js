a56a3046e1e5a5d006c6ce93fd3fbc24066f07bd
javiercastro5 <javier.c@fullsail.edu>
2014-04-09 21:42:34 -0400

    String Function
    Began work on String Function portion of Project 2

--- Castro_Javier_Project2.js
+++ Castro_Javier_Project2.js
@@ -1,1 +1,15 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+//Author: Javier Castro
+//Date: April 10th 2014
+//SDI 1404 Project 2
+
+
+var strGreetDog = function (dogName, timeOfDay) {
+    greeting = "Hello " + dogName + " it's a wonderful " + timeOfDay + "!!!"; 
+    return greeting;
+
+}; // string function
+
+var greetingDog = strGreetDog("Bobby", "afternoon");
+console.log(greetingDog);
